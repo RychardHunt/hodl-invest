e55acf9ca7422c43f2baa68b11129f8a2a71ef1d
RychardHunt <RychardHunt@users.noreply.github.com>
2018-02-18 02:19:40 -0500

    Progressing in React Native

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+
+function Welcome(props) {
+  return <h1> Hello and Welcome, {props.name} </h1>;
+  // highlight-next-line
+}
+const element = <Welcome name="Rychard" />;
+ReactDOM.render(element, document.getElementById('root'));
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" id = "root">
+      // <header className="App-header">
+      // <h1 className="App-title">Welcome to React</h1>
+      // </header>
+      // <p className="App-intro">
+      // To get started, edit <code>src/App.js</code> and save to reload.
+      // </p>
       </div>
     );
   }
 }
+
 
 export default App;
