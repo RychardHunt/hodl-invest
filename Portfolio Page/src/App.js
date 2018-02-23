4874bd0740b7830de26226b5ca80a315b7954b2a
aaronlin <alin33@buffalo.edu>
2018-02-23 04:28:36 -0500

    Chart.js implementation (React)
    Complete integration of chart.js (line, bar & pie graph visuals) set up
    for web app use. (react)

---
+++ Portfolio Page/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chart from './components/Chart';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['BTC', 'ETH', 'LTC', 'XLM', 'XRP', 'ARK'],
+        datasets:[
+            ]
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <Chart chartData={this.state.chartData} location="" legendPosition="bottom"/>
+      </div>
+    );
+  }
+}
+
+export default App;
