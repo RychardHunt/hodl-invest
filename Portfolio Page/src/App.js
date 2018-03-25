60759dd358c867fef65520c8063ab3aa5270c297
aaronlin <alin33@buffalo.edu>
2018-03-25 10:30:45 -0400

    changed pie graph to line graph
    Creation of historical data from json api grab

--- Portfolio Page/src/App.js
+++ Portfolio Page/src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Chart from './components/Chart';
 
+var url = 'https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=10'
+
+function Get(url){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",url,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
+}
+
+var json_obj = JSON.parse(Get(url));
+
 class App extends Component {
   constructor(){
     super();
@@ -15,12 +26,28 @@ class App extends Component {
     this.getChartData();
   }
 
+  // var datareq = new XMLHttpRequest();
+
   getChartData(){
     // Ajax calls here
     this.setState({
       chartData:{
         labels: ['BTC', 'ETH', 'LTC', 'XLM', 'XRP', 'ARK'],
         datasets:[
+          {
+            label:'Percentage',
+            data:[
+              20,
+              20,
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
             ]
           }
         ]
@@ -33,7 +60,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Your Hodl Invest Portfolio</h2>
         </div>
         <Chart chartData={this.state.chartData} location="" legendPosition="bottom"/>
       </div>
