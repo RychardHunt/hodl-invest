afdfa99a8f567681f3a6395a1dbc0f1262bbdb8b
usersblock <thanhphuocly@hotmail.com>
2018-03-28 20:46:59 -0400

    Update Login.jsx

--- client/hodl-invest/src/components/Login.jsx
+++ client/hodl-invest/src/components/Login.jsx
@@ -5,15 +5,15 @@ import './Login.css';
 const LoginPage = (props) => {
   const signupWasClickedCallback = (data) => {
     console.log(data);
-    alert('Signup callback, see log on the console to see the data.');
+    alert('Signup callback, see log on the console to see the data. testing');
   };
   const loginWasClickedCallback = (data) => {
     console.log(data);
-    alert('Login callback, see log on the console to see the data.');
+    alert('Login callback, see log on the console to see the data. testing');
   };
   const recoverPasswordWasClickedCallback = (data) => {
     console.log(data);
-    alert('Recover password callback, see log on the console to see the data.');
+    alert('Recover password callback, see log on the console to see the data. testing');
   };
   return (
     <div>
