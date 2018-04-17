df66711136c04f1df6b76db5a50fb507463ee621
Luis <luis815@outlook.com>
2018-04-17 11:01:47 -0400

    Added dynamic functionality

--- client/hodl-invest/src/components/BuySellPanel.jsx
+++ client/hodl-invest/src/components/BuySellPanel.jsx
@@ -11,8 +11,103 @@ class BuySellPanel extends Component {
             buyInput: '',
             sellInput: '',
             buySelect: 'btc',
-            sellSelect: 'btc'
+            sellSelect: 'btc',
+            btcPrice: 0,
+            ethPrice: 0
+        };
+        this.getBtcValue();
+        this.getEthValue();
+    }
+
+    static validateInput(input) {
+        //Only numbers and periods
+        for(let i = 0; i < input.length; ++i) {
+            let currentChar = input.charAt(i);
+            if(!((currentChar >= '0' && currentChar <= '9') || currentChar === '.')) {
+                return false;
+            }
         }
+
+        //Only one period
+        let hasPeriod = false;
+        for(let i = 0; i < input.length; ++i) {
+            let currentChar = input.charAt(i);
+            if(currentChar === '.') {
+                if(hasPeriod) {
+                    return false;
+                } else {
+                    hasPeriod = true;
+                }
+            }
+        }
+        return true;
+    }
+
+    coinToUsd(input,ticker) {
+        //Buying ONLY
+        if(input === '' || input === '.') {
+            return '0';
+        }
+        let parsedInput = parseFloat(input);
+        if(ticker === "btc") {
+            return '' + ((parsedInput * this.state.btcPrice).toFixed(2));
+        } else {
+            return '' + ((parsedInput * this.state.ethPrice).toFixed(2));
+        }
+    }
+
+    getBtcValue() {
+        let buySellPanel = this;
+        let data = null;
+        let xhr = new XMLHttpRequest();
+        xhr.withCredentials = false;
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                buySellPanel.setState({
+                    BuySelected : buySellPanel.state.isBuySelected,
+                    usdAmount: buySellPanel.state.usdAmount,
+                    coinAmount: buySellPanel.state.coinAmount,
+                    buyInput: buySellPanel.state.buyInput,
+                    sellInput: buySellPanel.state.sellInput,
+                    buySelect: buySellPanel.state.buySelect,
+                    sellSelect: buySellPanel.sellSelect,
+                    btcPrice: parseFloat(this.responseText),
+                    ethPrice: buySellPanel.state.ethPrice
+                })
+            }
+        });
+        xhr.open("GET", "https://hodl-invest-server.herokuapp.com/api/v1/cryptocoins/btc");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.setRequestHeader("Postman-Token", "daead1f1-6344-42b5-89dc-92981df21b62");
+        xhr.send(data);
+    }
+
+    getEthValue() {
+        let buySellPanel = this;
+        let data = null;
+        let xhr = new XMLHttpRequest();
+        xhr.withCredentials = false;
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                buySellPanel.setState({
+                    BuySelected : buySellPanel.state.isBuySelected,
+                    usdAmount: buySellPanel.state.usdAmount,
+                    coinAmount: buySellPanel.state.coinAmount,
+                    buyInput: buySellPanel.state.buyInput,
+                    sellInput: buySellPanel.state.sellInput,
+                    buySelect: buySellPanel.state.buySelect,
+                    sellSelect: buySellPanel.sellSelect,
+                    btcPrice: buySellPanel.state.btcPrice,
+                    ethPrice: parseFloat(this.responseText)
+                })
+            }
+        });
+        xhr.open("GET", "https://hodl-invest-server.herokuapp.com/api/v1/cryptocoins/eth");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Cache-Control", "no-cache");
+        xhr.setRequestHeader("Postman-Token", "daead1f1-6344-42b5-89dc-92981df21b62");
+        xhr.send(data);
     }
 
     displayBuy(){
@@ -23,7 +118,9 @@ class BuySellPanel extends Component {
             buyInput: this.state.buyInput,
             sellInput: '',
             buySelect: this.state.buySelect,
-            sellSelect: 'btc'
+            sellSelect: 'btc',
+            btcPrice: this.state.btcPrice,
+            ethPrice: this.state.ethPrice
         });
     }
 
@@ -35,104 +132,75 @@ class BuySellPanel extends Component {
             buyInput: '',
             sellInput: this.state.sellInput,
             buySelect: 'btc',
-            sellSelect: this.state.sellSelect
+            sellSelect: this.state.sellSelect,
+            btcPrice: this.state.btcPrice,
+            ethPrice: this.state.ethPrice
         })
     }
 
     handleBuySelect(event) {
         this.setState({
             isBuySelected : this.state.isBuySelected,
-            usdAmount: this.state.usdAmount,
+            usdAmount: this.coinToUsd(this.state.buyInput,event.target.value),
             coinAmount: this.state.coinAmount,
             buyInput: this.state.buyInput,
             sellInput: this.state.sellInput,
             buySelect: event.target.value,
-            sellSelect: this.state.sellSelect
-        })
+            sellSelect: this.state.sellSelect,
+            btcPrice: this.state.btcPrice,
+            ethPrice: this.state.ethPrice
+        });
     }
 
     handleSellSelect(event) {
         this.setState({
             isBuySelected : this.state.isBuySelected,
             usdAmount: this.state.usdAmount,
-            coinAmount: this.state.coinAmount,
+            coinAmount: this.coinToUsd(this.state.sellInput,event.target.value),
             buyInput: this.state.buyInput,
             sellInput: this.state.sellInput,
             buySelect: this.state.buySelect,
-            sellSelect: event.target.value
-        })
-    }
-
-    validateInput(input) {
-        for(let i = 0; i < input.length; ++i) {
-            let currentChar = input.charAt(i);
-            if(!((currentChar >= '0' && currentChar <= '9') || currentChar === '.')) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    getCoinValue(ticker) {
-        var data = null;
-
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = false;
-
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-                console.log(this.responseText);
-            }
+            sellSelect: event.target.value,
+            btcPrice: this.state.btcPrice,
+            ethPrice: this.state.ethPrice
         });
-
-        xhr.open("GET", "https://hodl-invest-server.herokuapp.com/api/v1/cryptocoins/eth");
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("Cache-Control", "no-cache");
-        xhr.setRequestHeader("Postman-Token", "daead1f1-6344-42b5-89dc-92981df21b62");
-
-        xhr.send(data);
-    }
-
-    convertToUsd(ticker,coinAmount) {
-
-    }
-
-    convertToCoin(ticker,usdAmount) {
-
     }
 
     handleBuyInput(event) {
         let input = event.target.value;
-        if(!this.validateInput(input)) {
+        if(!BuySellPanel.validateInput(input)) {
             return
         }
         //Update state
         this.setState({
-            sBuySelected : this.state.isBuySelected,
-            usdAmount: this.state.usdAmount,
+            isBuySelected : this.state.isBuySelected,
+            usdAmount: this.coinToUsd(input,this.state.buySelect),
             coinAmount: this.state.coinAmount,
             buyInput: input,
             sellInput: this.state.sellInput,
             buySelect: this.state.buySelect,
-            sellSelect: event.target.value
-        })
+            sellSelect: this.state.sellSelect,
+            btcPrice: this.state.btcPrice,
+            ethPrice: this.state.ethPrice
+        });
     }
 
     handleSellInput(event) {
-        this.getCoinValue('btc');
         let input = event.target.value;
-        if(!this.validateInput(input)) {
+        if(!BuySellPanel.validateInput(input)) {
             return
         }
 
         this.setState({
-            sBuySelected : this.state.isBuySelected,
+            isBuySelected : this.state.isBuySelected,
             usdAmount: this.state.usdAmount,
-            coinAmount: this.state.coinAmount,
+            coinAmount: this.coinToUsd(input,this.state.sellSelect),
             buyInput: this.state.buyInput,
             sellInput: input,
             buySelect: this.state.buySelect,
-            sellSelect: event.target.value
+            sellSelect: this.state.sellSelect,
+            btcPrice: this.state.btcPrice,
+            ethPrice: this.state.ethPrice
         })
     }
 
@@ -145,9 +213,9 @@ class BuySellPanel extends Component {
                     <option value="eth">ETH</option>
                 </select>
                 <br/>
-                Enter amount: <input value={this.state.buyInput} onChange={this.handleBuyInput.bind(this)}/>
+                Enter coin amount: <input value={this.state.buyInput} onChange={this.handleBuyInput.bind(this)}/>
                 <br/>
-                Total: {this.state.coinAmount} CoinType
+                Total cost: ${this.state.usdAmount}
                 <br/>
                 <button onClick={null}>Buy</button>
             </form>
@@ -159,16 +227,14 @@ class BuySellPanel extends Component {
                     <option value="eth">ETH</option>
                 </select>
                 <br/>
-                Enter amount: <input value={this.state.sellInput} onChange={this.handleSellInput.bind(this)}/>
+                Enter coin amount: <input value={this.state.sellInput} onChange={this.handleSellInput.bind(this)}/>
                 <br/>
-                Total: ${this.state.usdAmount}
+                Total value: ${this.state.coinAmount}
                 <br/>
                 <button onClick={null}>Sell</button>
             </form>
         }
     }
-
-
 
     render() {
         return (
